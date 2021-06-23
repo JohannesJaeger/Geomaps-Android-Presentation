40f4591344706597df52594b0d6be4c53710d699
JohannesJaeger <mail@jaeger-johannes.de>
2021-06-23 16:08:00 +0200

    Add side bar tool bar (wip)

--- app/src/main/java/edu/hm/mobile_app/geomaps/integrated_maps/MapsActivity.kt
+++ app/src/main/java/edu/hm/mobile_app/geomaps/integrated_maps/MapsActivity.kt
@@ -2,13 +2,18 @@ package edu.hm.mobile_app.geomaps.integrated_maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import edu.hm.mobile_app.geomaps.R
 
 class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_maps)
+        setSupportActionBar(toolbar)
+
         supportFragmentManager.beginTransaction().replace(R.id.maps_frame_layout, MapsFragment()).commit()
     }
 }
