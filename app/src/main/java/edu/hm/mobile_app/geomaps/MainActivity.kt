7c5d6a0d77358bb5fb1103d558f9f12e98287a6c
JohannesJaeger <mail@jaeger-johannes.de>
2021-06-24 19:39:08 +0200

    Add comments

--- app/src/main/java/edu/hm/mobile_app/geomaps/MainActivity.kt
+++ app/src/main/java/edu/hm/mobile_app/geomaps/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //  Streetview in Marienplatz via Geolocations
         findViewById<Button>(R.id.munich_streetview_button).setOnClickListener {
             val uri = Uri.parse("google.streetview:cbll=48.1371349,11.5754819")
             val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Show Map of Munich via Geolocations and Zoom level
         findViewById<Button>(R.id.munich_map_button).setOnClickListener {
             val uri = Uri.parse("geo:48.1371349,11.5754819?z=18")
             val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -27,13 +29,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Show Map of Hochschule München via Search Query
         findViewById<Button>(R.id.hochschule_münchen_map_button).setOnClickListener {
-            val uri = Uri.parse("geo:0,0?q=Hochschule+München+Lotstraße")
+            val uri = Uri.parse("geo:0,0?q=Hochschule+München+Lothstraße")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             intent.setPackage("com.google.android.apps.maps")
             startActivity(intent)
         }
 
+        // Shows public transport around the Geolocations of the Hochschule München
         findViewById<Button>(R.id.hochschule_münchen_public_transport_map_button).setOnClickListener {
             val uri = Uri.parse("geo:48.1538893,11.5544265?z=16&q=public+transport")
             val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -41,6 +45,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Shows restaurants around the Geolocations of the center of munich
         findViewById<Button>(R.id.munich_restaurant_map_button).setOnClickListener {
             val uri = Uri.parse("geo:48.1371349,11.5754819?z=15&q=restaurant")
             val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -48,6 +53,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Bicycle Routing to Hochschule München from current location
         findViewById<Button>(R.id.basic_route_button).setOnClickListener {
             val uri = Uri.parse("google.navigation:q=Hochschule+München+Lotstraße&mode=b")
             val intent = Intent(Intent.ACTION_VIEW, uri)
@@ -55,8 +61,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Advanced walking routing from custom location to Hochschule München with stopovers
         findViewById<Button>(R.id.advanced_route_button).setOnClickListener {
-            val uri = Uri.parse("https://www.google.com/maps/dir/?api=1&dir_action=navigate&origin=48.1680391,11.7178413&destination=48.1546997,11.5567071&waypoints=48.135,11.581|48.1888204,11.6408373&travelmode=walking")
+            val uri =
+                Uri.parse("https://www.google.com/maps/dir/?api=1&dir_action=navigate&origin=48.1680391,11.7178413&destination=48.1546997,11.5567071&waypoints=48.135,11.581|48.1888204,11.6408373&travelmode=walking")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             intent.setPackage("com.google.android.apps.maps")
             startActivity(intent)
